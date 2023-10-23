fb82f95a7f0dfc3065c157957c0a9ca35eb2b3cf
Suchanun <suchanun_sir@cmu.ac.th>
2023-10-23 19:02:52 +0700

    update position

--- src/main/java/se331/lab/rest/Config/InitApp.java
+++ src/main/java/se331/lab/rest/Config/InitApp.java
@@ -22,10 +22,13 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115001")
                 .image("https://live.staticflickr.com/65535/53133553491_793e26edb5_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
+
 
         userRepository.save(User.builder()
 
@@ -36,10 +39,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115002")
                 .image("https://live.staticflickr.com/65535/53134034918_f64107b2ff_o.png")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -50,10 +55,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115003")
                 .image("https://live.staticflickr.com/65535/53133560466_166806bf86_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -64,10 +71,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115004")
                 .image("https://live.staticflickr.com/65535/53132961792_5b6bede0a8_o.png")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -78,10 +87,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115005")
                 .image("https://live.staticflickr.com/65535/53133968840_72266c8328_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -92,10 +103,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115006")
                 .image("https://live.staticflickr.com/65535/53133989210_ac3281d585_o.png")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -106,10 +119,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115007")
                 .image("https://live.staticflickr.com/65535/53134061948_4e464cdf6b_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -120,10 +135,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115008")
                 .image("https://live.staticflickr.com/65535/53134061988_7e6a970d77_o.png")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -134,10 +151,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115009")
                 .image("https://live.staticflickr.com/65535/53134062198_7f71d8a07c_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -148,10 +167,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115010")
                 .image("https://live.staticflickr.com/65535/53132982987_dd0dbdc077_o.png")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -162,10 +183,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115011")
                 .image("https://live.staticflickr.com/65535/53134062193_b33112ef12_o.png")
                 .role("student")
-                .advisorName("Prof. Takuro")
+                .advisorName("Takuro")
                 .advisorSurname("Oiwake")
                 .advisorImage("https:live.staticflickr.com/65535/53133989675_cb25b69df0_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -176,10 +199,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115012")
                 .image("https://live.staticflickr.com/65535/53132982982_859c79a363_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -190,10 +215,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115013")
                 .image("https://live.staticflickr.com/65535/53132982967_f1a812754a_o.png")
                 .role("student")
-                .advisorName("Prof. Takuro")
+                .advisorName("Takuro")
                 .advisorSurname("Oiwake")
                 .advisorImage("https:live.staticflickr.com/65535/53133989675_cb25b69df0_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -204,10 +231,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115014")
                 .image("https://live.staticflickr.com/65535/53133789474_2f944bd92f_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -218,10 +247,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115015")
                 .image("https://live.staticflickr.com/65535/53133989540_ab0dfeca3d_o.png")
                 .role("student")
-                .advisorName("Prof. Takuro")
+                .advisorName("Takuro")
                 .advisorSurname("Oiwake")
                 .advisorImage("https:live.staticflickr.com/65535/53133989675_cb25b69df0_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -232,10 +263,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115016")
                 .image("https://live.staticflickr.com/65535/53277085611_dd0b175328_o.jpg")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -246,10 +279,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115017")
                 .image("https://live.staticflickr.com/65535/53277455639_48b0283f0b_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -260,10 +295,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115018")
                 .image("https://live.staticflickr.com/65535/53277138646_af3225fb6d_o.jpg")
                 .role("student")
-                .advisorName("Prof. Yasufumi")
+                .advisorName("Yasufumi")
                 .advisorSurname("Nekomata")
                 .advisorImage("https:live.staticflickr.com/65535/53133764254_86716f4c97_z.jpg")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -274,10 +311,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115019")
                 .image("https://live.staticflickr.com/65535/53277467694_6a0a64f070_o.png")
                 .role("student")
-                .advisorName("Prof. Takuro")
+                .advisorName("Takuro")
                 .advisorSurname("Oiwake")
                 .advisorImage("https:live.staticflickr.com/65535/53133989675_cb25b69df0_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
@@ -288,10 +327,12 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .studentID("642115020")
                 .image("https://live.staticflickr.com/65535/53276245422_260bea1647_o.png")
                 .role("student")
-                .advisorName("Prof. Keishin")
+                .advisorName("Keishin")
                 .advisorSurname("Ukai")
                 .advisorImage("https:live.staticflickr.com/65535/53133761344_316fb142a6_o.png")
-                .department("CAMT").build());
+                .department("CAMT")
+                .position("Prof.")
+                .build());
 
         userRepository.save(User.builder()
 
